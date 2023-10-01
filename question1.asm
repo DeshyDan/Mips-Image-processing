52e818cdf792a70c7e9fd5a09c25fb406af1912c
Deshy Dan <deshy733@gmail.com>
2023-10-01 12:26:57 +0200

    Build increasing brightness

--- question1.asm
+++ question1.asm
@@ -1,19 +1,17 @@
- .data
+.data
     #  input file
-    inputFile: .asciiz "C:/Users/Deshy Dan/OneDrive - University of Cape Town/2023/2nd semester/CSC2002S/Mips Image processing/test.txt" 
-    inputText: .space 4096
-    headerInfo: .space 1000
+    inputFile: .asciiz "C:/Users/Deshy Dan/OneDrive - University of Cape Town/2023/2nd semester/CSC2002S/Mips Image processing/images/house_64_in_ascii_crlf.ppm" 
+    inputText: .space 80000
+    headerInfo: .space 19
     outputText: .space 1000
     # output file
-    outputFile:.asciiz "C:/Users/Deshy Dan/OneDrive - University of Cape Town/2023/2nd semester/CSC2002S/Mips Image processing/out.txt" 
-    outputBuffer: .space 4096
-    bufferLength: .word 100
+    outputFile:.asciiz "C:/Users/Deshy Dan/OneDrive - University of Cape Town/2023/2nd semester/CSC2002S/Mips Image processing/hello.ppm" 
     # descriptors
     inputFileDescriptor: .word 0   
     outputFileDescriptor: .word 0 
     # pixel totals 
-    oldPixelTotal: .word 0   # allocate a word for the old pixel total
-    newPixelTotal: .word 0   # allocate a word for the new pixel total
+    oldPixelTotal: .word 1   # allocate a word for the old pixel total
+    newPixelTotal: .word 1   # allocate a word for the new pixel total
 
 .text 
 main:
@@ -41,17 +39,54 @@ main:
     li $v0 , 14
     move $a0 , $t0 
     la $a1, inputText
-    li $a2, 4096
+    li $a2, 80000
     syscall
 
-
+    la $t5 , inputText # input text contianer
     # Todo: Add code for printing the header
+    # Reading and writing the header
+    li $t4 , 0 # counter
+    la $t6, headerInfo
 
+readHeader:
+   beq $t4, 4, endReadHeader # when finished looping over header information
+    lb $s0, 0($t5)     # load byte at memory address $t5 into $s0
+    sb $s0, ($t6)      # store the character in $s0 at the memory address pointed to by $t6
+    # print character in $so 
+    li $v0, 11           # load syscall for print_character
+    move $a0, $s0        # move the character to $a0
+    syscall
+    
+    addi $t5, $t5, 1   # increment $t5 to move to next byte in memory
+beq $s0, 10, writeHeader # checking for end of line
+    addi $t6, $t6, 1   # increment $t6 to move to next byte in memory
+    
+    j readHeader
+    
+  
+
+writeHeader:
+    addi $t6, $t6, 1   # increment $t6 to move to next byte in memory
+    li $t2, 0x0A            # Load the ASCII code for the newline character into $t2
+    sb $t2, ($t6)         # Store the newline character in the buffer
+
+    addi $t4, $t4 , 1 # increment counter
+   
+    j readHeader
+
+
+endReadHeader:
+sb $zero, ($t6)          # Store the null-terminate character in the string
+ lw $t0, outputFileDescriptor 
+    li $v0, 15          # system call for writing to a file
+    move $a0, $t0    # move file descriptor to $a0
+    la $a1, headerInfo     # load address of buffer to $a1
+    li $a2, 18          # write 4 bytes (assuming $t3 contains a word)
+    syscall             # write to the file
 ######################################pixel calculation project######################################
     li $t2 , 0 # original pixel total
     li $t3 , 0 # new pixel total
     li $t4 , 0 # counter
-    la $t5 , inputText # input text contianer
     move $t6, $zero # stores the integer
     li $t7 , 10
     
@@ -61,13 +96,14 @@ computeLines:
     
     lb $s0 , 0($t5)    
     addi $t5 , $t5, 1
-    
+    beq $t4 , 12288 , endComputeLines
+    beq $s0 , $zero , endComputeLines # when finished looping over each pixel
     beq $s0 , 10, computePixel  # checks for end of line
-  
+    beq $s0, -1, endComputeLines 
     li $v0, 11           # load syscall for print_character
     move $a0, $s0        # move the character to $a0
     syscall  
-    beq $s0 , $zero , endComputeLines # when finished looping over each pixel
+    
     addi $s1, $a0, -48
     mul $t6, $t6, $t7
     add $t6, $t6, $s1
@@ -83,6 +119,14 @@ computePixel:
     
     add $t2 , $t2 , $t6  # adding to the original pixel total
     addi $t6 , $t6 , 10  
+    bgt $t6, 255, limitValue
+    j continueExecution
+
+    limitValue:
+    li $t6, 255
+
+continueExecution:
+
     add $t3 , $t3 , $t6 # addding to the new pixel total
      # save the old and new pixel totals in memory
     sw $t2, oldPixelTotal  # store the old pixel total in memory
@@ -91,6 +135,17 @@ computePixel:
     li $v0 , 1
     move $a0 , $t6 
     syscall
+    nop 
+    nop 
+    bgt $t6, 100, greaterThanHundred
+    li $t9 ,3
+    j intToString
+
+greaterThanHundred:
+    li $t9 , 4
+    j intToString
+
+    intToString:
     # #####################Turning $t6 into string####################
     # Allocate space for the string
     li $v0, 9          # Load syscall for sbrk
@@ -111,60 +166,23 @@ intToString_loop:
     addi $t3, $t3, -1   # Move to the next character
     mflo $t6
     bnez $t6, intToString_loop  # Repeat until $t6 is zero
-
-# addi $t3, $t3, 1       # Move to the next character
-
-# # Null-terminate the string
-# la $t2, $zero              # Null-terminate character (ASCII value 0)
-sb $zero, ($t3)          # Store the null-terminate character in the string
-
-# copy_loop:
-#     lb $t2, ($t0)         # Load a character from the string
-#     sb $t2, ($t3)         # Store the character in the buffer
-#     addi $t0, $t0, 1      # Move to the next character in the string
-#     addi $t3, $t3, 1      # Move to the next character in the buffer
-#     beqz $t2, copy_done   # If we've reached the null-terminator, we're done
-#     j copy_loop
-
+# sb $zero, ($t3)          # Store the null-terminate character in the string
 copy_done:
    li $v0 , 4
     move $a0 , $t3
     syscall
-    li $t2, 10            # Load the ASCII code for the newline character into $t2
+    li $t2, 0x0A            # Load the ASCII code for the newline character into $t2
     sb $t2, ($t3)         # Store the newline character in the buffer
     li $v0 , 4
     move $a0 , $t3
     syscall
-
-    # Load the address of the output buffer into $a0
-    la $a0, outputBuffer
+    lw $t0, outputFileDescriptor 
+    li $v0, 15          # system call for writing to a file
+    move $a0, $t0    # move file descriptor to $a0
+    la $a1, ($t3)       # load address of buffer to $a1
+    move $a2, $t9           # write 4 bytes (assuming $t3 contains a word)
+    syscall             # write to the file
     
-    # # Load the length of the buffer into $a1
-    # la $a1, bufferLength
-
-    # # Load the current length of the buffer into $a2
-    # la $t0, bufferLength
-    # sub $a2, $t0, $a1
-
-    # # Add the length of the value to append to the current length of the buffer
-    # add $a1, $a1, $t3
-
-    # # Append the value to the buffer
-    # add $a2, $a2, $t3
-
-  
-
-
-
-   
-
-    ################Writing to the output file############################
-    # li $v0, 15
-    # move $a0 , $t1
-    # move $a1 , $t6
-    # li $a2, 4
-    # syscall
-    ######################################################################
     move $t6, $zero # restoring the integer back to 0 
         # load the old and new pixel totals from memory
     lw $t2, oldPixelTotal  # load the old pixel total from memory
@@ -173,30 +191,9 @@ copy_done:
 
 endComputeLines:
 
-    lw $t1, outputFileDescriptor 
-
-    li $v0, 15        # load syscall for write
-    move $a0,$t1      # move the file descriptor to $a0
-    la $a1, outputBuffer    # move the address of the string to $a1
-    li $a2, 4096         # set $a2 to 4 to indicate writing 32 bytes
-    syscall
-    li $v0, 1
-    move $a0 , $t6
-    syscall
+   
     j closeFiles
-######################################String to integer function  ######################################
-    
-# stringToInt:
-#     lb $a0 , 0($s0)
-#     addi $t5 , $t5, 1
-#     beq $a0, 0, endStringToInt
-#     addi $s1, $a0, -48
-#     mul $t6, $t6, $t7
-#     add $t6, $t6, $s1
-#     j stringToInt
 
-
-############################################################################################################
     
 
 closeFiles:
@@ -216,4 +213,3 @@ closeFiles:
 exit:
     li $v0 , 10 
     syscall
-    
